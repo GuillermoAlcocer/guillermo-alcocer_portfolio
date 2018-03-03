667f3315ecf71ccc2a1c2940f249c7944c482e0c
Guillermo Alcocer <g.alcocer.s@gmail.com>
2018-03-03 15:32:32 +0100

    project update

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,8 @@
-// $(document).ready(function () {
-//     var $horizontal = $('#horizontal');
-//
-//     $(window).scroll(function () {
-//         var s = $(this).scrollTop(),
-//             d = $(document).height(),
-//             c = $(this).height();
-//
-//         scrollPercent = (s / (d + c)*10);
-//
-//         var position = (scrollPercent * ($(document).width() - $horizontal.width()));
-//
-//         $horizontal.css({
-//             'left': position
-//         });
-//     });
-// });
+
 var window_width = $(window).width();
 
 $(window).scroll(function () {
 	var scroll_position = $(window).scrollTop();
 	var object_position_left = scroll_position*(scroll_position/window_width);
-	$('#horizontal').css({'left':-object_position_left});
+	$('#scroll-horizontal').css({'marginLeft':-object_position_left});
 });
