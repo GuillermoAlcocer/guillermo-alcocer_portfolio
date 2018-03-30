a1b6e680c1f48fe1642594062824c7e1c161e28e
guillermoalcocer <g.alcocer.s@gmail.com>
2018-03-31 01:12:56 +0200

    homepage sectionpage

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,9 @@
-
 var window_width = $(window).width();
 
-$(window).scroll(function () {
-	var scroll_position = $(window).scrollTop();
-	var object_position_left = scroll_position*(scroll_position/window_width);
-	$('#scroll-horizontal').css({'marginLeft':-object_position_left});
+$(window).scroll(function() {
+  var scroll_position = $(window).scrollTop();
+  var object_position_left = scroll_position * (scroll_position / window_width);
+  $('#scroll-horizontal').css({
+    'marginLeft': -object_position_left
+  });
 });
